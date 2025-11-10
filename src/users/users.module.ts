b2607fb19966e0a8e27bb9da02eb3eb46a6d9934
Nimatullah Razmjo <nimatullah.razmjo@gmail.com>
2025-11-10 13:27:22 -0500

    Add User and Report Entity

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/users/user.entity';
 
 @Module({
-  controllers: [UsersController],
+  imports:[
+    TypeOrmModule.forFeature([User])
+  ],
+  controllers: [
+    UsersController],
   providers: [UsersService]
 })
 export class UsersModule {}
